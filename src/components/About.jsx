1b33694e7210bc34abded8732a34f514b7982d2e
Gion Sunday <richardadaji@gmail.com>
2025-04-14 10:43:40 +0000

    trfj

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className="App">
+    <header className="App-header">
+    <h1>About Section</h1>
+    
+    </header>
+  </div>
+  )
+}
