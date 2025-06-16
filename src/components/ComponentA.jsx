0467d0392c7cf07df890d27d3c1610ca2c43b8b9
gionsunday <richardadaji@gmail.com>
2025-06-16 22:50:57 +0100

    df

--- src/components/ComponentA.jsx
+++ src/components/ComponentA.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
 import { ComponentB } from './ComponentB'
+import { useState, useEffect } from 'react'
+
 
 export const ComponentA = () => {
+
+  const [count, setCount] = useState([])
+  const [effectTrigger, setCountEffectTrigger] = useState("")
+
+
+  useEffect(() => {
+
+    setCount([1, 2, 3, 4, 5])
+    console.log("useEffect called")
+  }, [effectTrigger])
+
+
+
   return (
     <div>
-        ComponentA 
+      <input type="text" onChange={(e) => { setCountEffectTrigger(e.target.value) }} />
+      <ul>
+        {
+          count.map((item, index) => (
+            <li key={index}>
+              {item}
+            </li>
+          ))
+        }
+      </ul>
 
-        <ComponentB />
+
+
+
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,10 @@
+import React from 'react';
+import { useContext } from 'react';
+import { userNameContext } from '../App';
+
 function Footer() {
+  const contextValue = useContext(userNameContext);
+  console.log(contextValue.userName)
   return (
     <footer className="bg-gray-800 text-white text-center p-4 mt-auto">
       <p>&copy; 2023 My Simple React App</p>
